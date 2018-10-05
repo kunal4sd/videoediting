d57f5126841101f60c46ab2d6390098ad4601ef5
ecrafti <craftieduard@gmail.com>
2018-10-05 13:44:05 +0300

    Added new front-end libraries in Yarn; compiled front-end resources with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var toolbox = require('gulp-toolbox');
+
+var config = {
+    production: true,
+    assets: {
+        enabled: true,
+        origin: 'app/resources',
+        target: 'public',
+        glob: [
+            'fonts/**',
+            'images/**',
+            'js/**/*.swf'
+        ]
+    },
+    js: {
+        enabled: true,
+        libs: {
+            origin: 'app/resources/js/libs',
+            target: 'public/js',
+            glob: [
+                'jquery.js',
+                '*.js'
+            ]
+        },
+        src: {
+            origin: 'app/resources/js',
+            target: 'public/js',
+            glob: [
+                'main.js'
+            ]
+        }
+    },
+    sync: {
+        enabled: true,
+        glob: [
+            'app/resources/templates/**/*',
+            'public/css/**/*',
+            'public/fonts/**/*',
+            'public/js/**/*'
+        ]
+    }
+};
+
+toolbox.init(gulp, config);
