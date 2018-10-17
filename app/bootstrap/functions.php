fc0096ee032e14b763feb77169948f990cd233a1
ecrafti <craftieduard@gmail.com>
2018-10-17 18:54:25 +0300

    Added check_mandatory_fields global function

--- app/bootstrap/functions.php
+++ app/bootstrap/functions.php
@@ -1,6 +1,26 @@
 <?php
 
 /**
+ * @param object $object : instance of class
+ * @param array $array : target array to check fields in
+ * @throws Exception : if const field in $object is not found in $array, throw Exception
+ * @return boolean true : only if everything checks, return true
+ */
+function check_mandatory_fields($object, array $array = [])
+{
+
+    $constants = get_all_constants($object);
+
+    foreach($constants as $const => $value) {
+        if (get_from_array($value, $array) === null) {
+            throw new Exception("Mandatory field `{$value}` not found in provided config.");
+        }
+    }
+
+    return true;
+}
+
+/**
  * @param string $needle   : searched field or field path
  * @param array  $haystack : associative array
  * @return mixed : found value, $default otherwise
