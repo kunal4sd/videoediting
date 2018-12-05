5a0425d2b9cba79ae916d3a7a61be83440625b4d
ecrafti <craftieduard@gmail.com>
2018-12-05 16:11:20 +0200

    Added enums class Videos

---
+++ src/Libs/Enums/Videos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Libs\Enums;
+
+class Videos
+{
+    const RAW_VIDEO_PATH = '/storage/recordings';
+    const RAW_VIDEO_FORMAT = 'ts';
+    const RAW_VIDEO_LENGTH = 5;
+    const PLAYLIST_PATH = 'tmp/playlist';
+    const POSTER_PATH = 'tmp/poster';
+    const MOVIE_PATH = 'userfiles/output/testOutput';
+    const PLAYLIST_FORMAT = 'm3u8';
+    const POSTER_FORMAT = 'jpg';
+    const MOVIE_FORMAT = 'mp4';
+    const STANDARD_SCANNING_PERIOD = 4; // RAW_VIDEO_LENGTH - 1
+    const EXTENDED_SCANNING_PERIOD = 3600;
+}
