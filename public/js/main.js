4b70423ed514cce45a682183ce822d072b4e1398
ecrafti <craftieduard@gmail.com>
2018-12-06 16:58:03 +0200

    Updated js to update video player only if it exists

--- public/js/main.js
+++ public/js/main.js
@@ -2061,23 +2061,35 @@ $( function() {
 
     var player = videojs.getPlayer('video-preview');
     var button_clear_player = $('#clear_player');
+    var max_tries = 25;
 
-    player.ready(function() {
-        var options = {
-            hidden: false,
-            responsive: true,
-            width: 800,
-            height: 400,
-            controlTime: true,
-            controlBar: {
-                volumePanel: {
-                    vertical: true
-                }
-            },
-        };
-        player.rangeslider(options);
+    var init_player = setInterval(function() {
 
-    });
+        if (player !== undefined) {
+            player.ready(function() {
+                var options = {
+                    hidden: false,
+                    responsive: true,
+                    width: 800,
+                    height: 400,
+                    controlTime: true,
+                    controlBar: {
+                        volumePanel: {
+                            vertical: true
+                        }
+                    },
+                };
+                player.rangeslider(options);
+
+            });
+            clearInterval(init_player);
+        }
+        else if (max_tries === 0) {
+            clearInterval(init_player);
+        }
+        max_tries--;
+    }, 200);
+
     button_clear_player.on('click', function(e) {
         e.preventDefault();
         global_functions.reset_player();
