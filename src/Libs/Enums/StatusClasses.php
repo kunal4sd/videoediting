60619e74c050b1a53ab4891dd2d455f9f98aa53e
ecrafti <craftieduard@gmail.com>
2018-12-10 16:54:55 +0200

    Added new Status and StatusClasses enums classes

---
+++ src/Libs/Enums/StatusClasses.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Libs\Enums;
+
+class StatusClasses
+{
+    const STATUS_NEW = 'success';
+    const STATUS_PENDING = 'warning';
+    const STATUS_APPROVE = 'info';
+    const STATUS_LIVE = 'secondary';
+}
