4a01115e69603cf6f5e0a18e755682e881c0d027
ecrafti <craftieduard@gmail.com>
2019-04-18 16:39:39 +0300

    Added check for cases where there is no login user activity

--- src/Modules/Core/Middleware/Authorization/SameSessionId.php
+++ src/Modules/Core/Middleware/Authorization/SameSessionId.php
@@ -7,21 +7,28 @@ use App\Modules\Abstracts\AbstractModule;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Libs\Enums\UserActivity;
+use \Exception;
 
 class SameSessionId extends AbstractModule
 {
     public function __invoke(Request $request, Response $response, $next)
     {
-        $user_activity_ar = $this->entity_user_activity->get_last_x_by_user_and_type(
-            1,
-            $this->session_user->get_user()->id,
-            UserActivity::LOGIN
-        );
 
-        if ($user_activity_ar->description === session_id()) {
-            return $next($request, $response);
+        try {
+
+            $user_activity_ar = $this->entity_user_activity->get_last_x_by_user_and_type(
+                1,
+                $this->session_user->get_user()->id,
+                UserActivity::LOGIN
+            );
+
+            if ($user_activity_ar->description === session_id()) {
+                return $next($request, $response);
+            }
         }
-        $this->logger->write(new \Exception('User session id has changed since login.', 403));
+        catch(Exception $e) {}
+
+        $this->logger->write(new Exception('User session id has changed since login.', 403));
 
         if ($request->getParam('ajax')) {
             return Json::build($response, [ 'authorization' => 'User authorization failed' ], 403);
