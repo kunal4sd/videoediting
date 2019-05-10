0b8eefffdb04490251934f5f13a79f1a0bac33b2
ecrafti <craftieduard@gmail.com>
2019-05-10 12:44:21 +0300

    Added new APP_PATH constant

--- app/bootstrap/constants.php
+++ app/bootstrap/constants.php
@@ -1,9 +1,10 @@
 <?php
 
 define("BASE_PATH", realpath(__DIR__ . '/../..'));
-define("PUBLIC_PATH", realpath(__DIR__ . '/../../public'));
-define("BIN_PATH", BASE_PATH . '/app/bin');
-define("CONFIG_FILE_PATH", BASE_PATH . '/app/config/config.php');
+define("PUBLIC_PATH", BASE_PATH . '/public');
+define("APP_PATH", BASE_PATH . '/app');
+define("BIN_PATH", APP_PATH . '/bin');
+define("CONFIG_FILE_PATH", APP_PATH . '/config/config.php');
 define("HOST", isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost');
 define(
     "SCHEME",
