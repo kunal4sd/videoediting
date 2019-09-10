68e0a2b9b4d04a3d4f01c1495f6b386345b6e41e
ecrafti <craftieduard@gmail.com>
2019-09-10 17:19:02 +0300

    Added confirmation dialog and dropdown to restart processes on each server

--- app/resources/js/publication.view.report.js
+++ app/resources/js/publication.view.report.js
@@ -2,29 +2,40 @@ $( function() {
 
     var table = $('#publications-report-table');
     var data_table = table.DataTable({select: false});
+    var modal_restart_process = $('#publication-modal-restart-process');
+    var restart_buttons = $('button[name="restart-process-btn"]');
 
     var activate_buttons = function() {
-        $('button[name="restart-process-btn"]').unbind('click').on('click', function() {
+        restart_buttons.unbind('click').on('click', function() {
+            modal_restart_process.trigger('publication.modal.open', this);
+        });
+    };
 
-            var btn = $(this);
+    modal_restart_process.on('publication.modal.open', function(e, btn) {
+        modal_restart_process.unbind('click').on('click', 'button[name="restart-btn"]', function(e) {
+
+            btn = $(btn);
             var data = btn.data();
 
+            modal_restart_process.modal('hide');
             global_functions.button_is_loading(btn);
             $.ajax({
                 method: 'post',
                 url: data.actionUrl,
                 data: {
                     id: data.id,
-                    process:data.process,
+                    process: data.process,
+                    server: data.server,
                     csrf_name: table.find('input[name="csrf_name"]').val(),
                     csrf_value: table.find('input[name="csrf_value"]').val()
                 },
                 complete: function (result) {
+                    event_emitter.trigger('form.ajax.result.alert', [result]);
                     global_functions.button_is_not_loading(btn);
                 }
             });
         });
-    };
+    });
 
     data_table.on('draw', function() { activate_buttons(); });
     data_table.order([4, 'asc'], [5, 'asc']).draw();
