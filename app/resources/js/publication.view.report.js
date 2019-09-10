dbad4523f87159a472fcc456a7159c783452c45a
ecrafti <craftieduard@gmail.com>
2019-09-10 13:03:11 +0300

    Reactivate buttons after data table redraw

--- app/resources/js/publication.view.report.js
+++ app/resources/js/publication.view.report.js
@@ -1,28 +1,31 @@
 $( function() {
 
     var table = $('#publications-report-table');
-    table.DataTable({select: false})
-        .order([4, 'asc'], [5, 'asc'])
-        .draw();
+    var data_table = table.DataTable({select: false});
 
-    $('button[name="restart-process-btn"]').on('click', function() {
+    var activate_buttons = function() {
+        $('button[name="restart-process-btn"]').unbind('click').on('click', function() {
 
-        var btn = $(this);
-        var data = btn.data();
+            var btn = $(this);
+            var data = btn.data();
 
-        global_functions.button_is_loading(btn);
-        $.ajax({
-            method: 'post',
-            url: data.actionUrl,
-            data: {
-                id: data.id,
-                process:data.process,
-                csrf_name: table.find('input[name="csrf_name"]').val(),
-                csrf_value: table.find('input[name="csrf_value"]').val()
-            },
-            complete: function (result) {
-                global_functions.button_is_not_loading(btn);
-            }
+            global_functions.button_is_loading(btn);
+            $.ajax({
+                method: 'post',
+                url: data.actionUrl,
+                data: {
+                    id: data.id,
+                    process:data.process,
+                    csrf_name: table.find('input[name="csrf_name"]').val(),
+                    csrf_value: table.find('input[name="csrf_value"]').val()
+                },
+                complete: function (result) {
+                    global_functions.button_is_not_loading(btn);
+                }
+            });
         });
-    });
+    };
+
+    data_table.on('draw', function() { activate_buttons(); });
+    data_table.order([4, 'asc'], [5, 'asc']).draw();
 });
