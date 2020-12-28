aad699ed106ad26782a827ef601fc31b6ca6d2b4
ecrafti <craftieduard@gmail.com>
2020-12-28 15:11:36 +0200

    Bug fixes

--- src/Modules/Video/Entities/Repository/Database/TextDB.php
+++ src/Modules/Video/Entities/Repository/Database/TextDB.php
@@ -34,37 +34,45 @@ class TextDB extends AbstractDatabase
             'end_time' => $end - $end_midnight,
             'publication_id' => [$publication_id, PDO::PARAM_INT]
         ];
+        $same_day = date("Y-m-d", $start) === date("Y-m-d", $end);
 
         $data = $this->db->fetch_all(
-            "
-                SELECT
-                    *
-                FROM pub_{$publication_id} AS texts
-                WHERE 1
-                    AND texts.pub_id = :publication_id
-                    AND (
-                        (
-                            texts.date = :start_day
-                            AND texts.start_time >= :start_time
-                        ) OR
-                        (
-                            texts.date = :end_day
-                            AND texts.start_time <= :end_time
-                        ) OR
-                        (
-                            texts.date > :start_day
-                            AND texts.date < :end_day
+            sprintf(
+                "
+                    SELECT
+                        *
+                    FROM pub_{$publication_id} AS texts
+                    WHERE 1
+                        AND texts.pub_id = :publication_id
+                        AND (
+                            (
+                                texts.date = :start_day
+                                AND texts.start_time >= :start_time
+                                %s
+                            ) OR
+                            (
+                                texts.date = :end_day
+                                %s
+                                AND texts.start_time <= :end_time
+                            ) %s
                         )
-                    )
-                ORDER BY
-                    CONCAT_WS(' ', texts.date, texts.start_time)
-                    ASC
-            ",
+                    ORDER BY
+                        CONCAT_WS(' ', texts.date, texts.start_time)
+                        ASC
+                ",
+                $same_day ? "AND texts.start_time <= :end_time" : '',
+                $same_day ? "AND texts.start_time >= :start_time" : '',
+                $same_day ? "" : "OR
+                    (
+                        texts.date > :start_day
+                        AND texts.date < :end_day
+                    )"
+            ),
             $params
         );
 
         foreach($data as $row) {
-            $result[] = new TextAR($row, $dir);
+            $result[] = new TextAR($row);
         }
 
         return $result;
