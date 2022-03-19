61a679030fc4cef5a5f2b44f89e7fa1a041d9761
zinger_pan <aperelyaev@gmail.com>
2022-03-19 14:54:53 +0600

    Search filters.

--- src/Modules/Publication/Entities/Repository/Database/CountryDB.php
+++ src/Modules/Publication/Entities/Repository/Database/CountryDB.php
@@ -4,6 +4,7 @@ namespace App\Modules\Publication\Entities\Repository\Database;
 
 use App\Modules\Abstracts\AbstractDatabase;
 use App\Modules\Publication\Entities\ActiveRecords\CountryAR;
+use PDO;
 
 class CountryDB extends AbstractDatabase
 {
@@ -35,24 +36,30 @@ class CountryDB extends AbstractDatabase
     }
 
     /**
-     * @param array $iso
+     * @param array $in_iso
      * @return CountryAR[]
      */
-    public function get_by_iso(array $iso): array
+    public function get_by_iso(array $in_iso): array
     {
         $result = [];
 
-        $data = $this->db->fetch_all(
-            "
-                SELECT
-                    *
-                FROM country
-                GROUP BY
-                    iso
-                ORDER BY
-                    name_en
-            "
-        );
+        foreach($in_iso as $iso) {
+            $key = "iso_{$iso}";
+            $params[$key] = [$iso, PDO::PARAM_STR];
+        }
+        $iso_str = ":iso_" . implode(', :iso_', $in_iso);
+        $sql = sprintf("
+                    SELECT
+                        *
+                    FROM country
+                    WHERE iso IN (%s)
+                    GROUP BY
+                        iso
+                    ORDER BY
+                        name_en
+                ", $iso_str);
+
+        $data = $this->db->fetch_all($sql, $params);
 
         foreach($data as $row) {
             $result[] = new CountryAR($row);
